746ce67ff7b3979987b8ed550ca4f60ffe10cede
srinivasan.thangavel <srinivasan.thangavel@niit.com>
2023-03-10 13:26:08 +0530

    boilerplate added

---
+++ ProductWebapp/src/main/java/com/stackroute/ProductWeapp/ProductWeappApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.ProductWeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWeappApplication.class, args);
+	}
+
+}
