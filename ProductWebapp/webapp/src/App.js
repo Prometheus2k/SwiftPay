061c817bccfc5bc7a980037c1693a06693fea1a2
kireeti <nittala.kireeti@batonsystems.com>
2023-03-16 14:35:46 +0530

    another transactionpage

--- ProductWebapp/webapp/src/App.js
+++ ProductWebapp/webapp/src/App.js
@@ -1,17 +1,20 @@
+import "./App.css";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SideBar from "./components/SideBar/SideBar";
 
-import './App.css';
-import Login from './components/Login/Login';
-import SignUp from './components/SignUp/SignUp';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import SideBar from './components/SideBar/SideBar';
-
-import Transfer from './Pages/Transfer';
+import Transfer from "./Pages/Transfer";
+import CustomPageTable from "./components/transactionHistory/AnotherTransactionHistory";
+import TransactionHistory from "./components/transactionHistory/TransactionHistory";
+import TDataGrid from "./components/transactionHistory/Trans3";
 
 function App() {
   return (
     <div className="App">
-      <Transfer />
-      
+      <TDataGrid />
+      <TransactionHistory />
+      <CustomPageTable />
     </div>
   );
 }
--- ProductWebapp/webapp/src/components/transactionHistory/TransactionHistory.jsx
+++ ProductWebapp/webapp/src/components/transactionHistory/TransactionHistory.jsx
@@ -9,8 +9,6 @@ export default function TransactionHistory() {
 
 
 
-
-
     return (
         <>
             <h1>Transaction History</h1>
