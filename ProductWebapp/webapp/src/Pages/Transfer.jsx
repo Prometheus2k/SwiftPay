40d00a2223f9a9e9ade8b5f18852fa43322d204c
Yohenba Kshetrimayum <yohenba.kshetrimayum@batonsystems.com>
2023-03-16 14:17:14 +0530

    transfer page ui, routing, storing and sending data

--- ProductWebapp/webapp/src/Pages/Transfer.jsx
+++ ProductWebapp/webapp/src/Pages/Transfer.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import "./Transfer.css";
 import arrow from "./arrow_right.svg"
+import axios from "axios"
+import TextField from '@mui/material/TextField';
 
 
 const Transfer = () => {
@@ -9,8 +11,8 @@ const Transfer = () => {
     data stored is account number, amount, desciption(message),account number, swift code of the bank, bank name, branch name
     */
     const [form, setForm] = useState({
-        account: "",
-        amount: 0,
+        accNumber: "",
+        amount: "",
         description: "",
         recieverAccNumber: "",
         swiftCode: "",
@@ -20,6 +22,21 @@ const Transfer = () => {
 
     const [recieverAccNumber, setRecieverAccNumber] = useState("")
 
+
+    const url = 'http://localhost:4000/transferData'
+
+    const addExpenses = () => {
+        axios.post(url, form).then(response => {
+            console.log(response)
+            console.log(response.status)
+            if (response.status == 201) {
+                alert("Transfer add added")
+            }
+        })
+            .catch(error => console.log(error))
+    }
+
+
     /*function to handle change in the input description and select field changed the values in the object according to the key defined*/
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -28,7 +45,7 @@ const Transfer = () => {
 
     const handleRecieverAccount = (e) => {
         setRecieverAccNumber(e.target.value)
-        
+
     }
 
     /* after submit shows the data stored in the form in alert and console */
@@ -42,21 +59,21 @@ const Transfer = () => {
             console.log(form);
             resetButton()
         }
-
+        addExpenses()
     }
 
     /*after submiting the form the values in the form is reset */
     const resetButton = () => {
         setForm({
             accNumber: "",
-            amount: 0,
+            amount: "",
             description: "",
             recieverAccNumber: "",
             swiftCode: "",
             bankName: "",
             branchName: ""
         });
-        setRecieverAccNumber()
+        setRecieverAccNumber("")
     }
 
     return (
@@ -67,12 +84,13 @@ const Transfer = () => {
                     <div className='transferForm_Left'>
                         <div className='row'>
                             <label>Account Number</label>
-                            <input type="number" name="accNumber" onChange={handleChange} value={form.accNumber} required/>
+                            <input type="number" name="accNumber" onChange={handleChange} value={form.accNumber} required />
+
                         </div>
 
                         <div className='row'>
                             <label>Amount</label>
-                            <input type="number" name="amount" onChange={handleChange} value={form.amount} required/>
+                            <input type="number" name="amount" onChange={handleChange} value={form.amount} required />
 
                         </div>
                         <div className='row'>
@@ -86,27 +104,34 @@ const Transfer = () => {
                     <div className='transferForm_Right'>
                         <div className='row'>
                             <label>Benefiatiary Account Number</label>
-                            <input type="number" name="recieverAccNumber" onChange={handleChange} value={form.recieverAccNumber} required/>
+                            <input type="number" name="recieverAccNumber" onChange={handleChange} value={form.recieverAccNumber} required />
                         </div>
 
                         <div className='row'>
                             <label>Confirm Benefiatiary Account Number</label>
-                            <input type="number" name="recieverAccNumber" onChange={handleRecieverAccount} value={recieverAccNumber} required/>
+                            <input type="number" name="recieverAccNumber" onChange={handleRecieverAccount} value={recieverAccNumber} required />
                         </div>
 
                         <div className='row'>
                             <label>Swift Code</label>
-                            <input type="text" name="swiftCode" onChange={handleChange} value={form.swiftCode} required/>
+                            <input type="text" name="swiftCode" onChange={handleChange} value={form.swiftCode} required />
                         </div>
 
                         <div className='row'>
                             <label>Bank Name</label>
-                            <input type="text" name="bankName" onChange={handleChange} value={form.bankName} required/>
+                            <select defaultValue="Savings" name="bankName" onChange={handleChange} value={form.bankName} >
+                                <option value="Choose...">Choose...</option>
+                                <option value="SBI">SBI</option>
+                                <option value="HDFC">HDFC</option>
+                                <option value="American Express">American Express</option>
+                                <option value="ICICI">ICICI</option>
+                                <option value="Axis">Axis</option>
+                            </select >
                         </div>
 
                         <div className='row'>
                             <label>Bank branch name</label>
-                            <input type="text" name="branchName" onChange={handleChange} value={form.branchName} required/>
+                            <input type="text" name="branchName" onChange={handleChange} value={form.branchName} required />
                         </div>
                     </div>
                 </div>
@@ -117,5 +142,6 @@ const Transfer = () => {
 }
 
 export default Transfer
+
 
 
