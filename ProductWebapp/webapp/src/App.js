72062754b9ef19eddb0a6f7b3f67bb24dbaabd6e
Prakhar Singh <prakhar.singh@batonsystems.com>
2023-03-19 15:57:35 +0530

    Fixed the Login Sign up pages

--- ProductWebapp/webapp/src/App.js
+++ ProductWebapp/webapp/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
 import Transfer from "./Pages/Transfer";
-
+import Login from './components/Login/Login'
+import SignUp from './components/SignUp/SignUp'
 import UserProfile from "./components/userProfile/UserProfile";
 import AddBankAccount from "./components/addbankaccount/AddBankAccount";
 import TransactionHistory from "./components/transactionHistory/TransactionHistory";
@@ -10,8 +11,10 @@ import TransactionHistory from "./components/transactionHistory/TransactionHisto
 function App() {
   return (
     <div className="App">
-      <SideBar />
       <Routes>
+      <Route path="/" element={<SideBar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/transfer" element={<Transfer />} />
         <Route path="/transactionhistory" element={<TransactionHistory />} />
         <Route path="/userprofile" element={<UserProfile />} />
