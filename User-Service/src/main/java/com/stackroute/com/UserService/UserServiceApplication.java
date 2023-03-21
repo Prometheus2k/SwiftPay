6cac2b117de3ec344c49184059110d9727671927
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-22 01:22:47 +0530

    Completed EurekaServer, Added Eureka Client in User-Service

--- User-Service/src/main/java/com/stackroute/com/UserService/UserServiceApplication.java
+++ User-Service/src/main/java/com/stackroute/com/UserService/UserServiceApplication.java
@@ -4,9 +4,11 @@ import com.stackroute.com.UserService.filter.UserFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
