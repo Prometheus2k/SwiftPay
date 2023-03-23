6271ba1116eb89af7ab09eca1ca167f00b20055c
kireeti <nittala.kireeti@batonsystems.com>
2023-03-23 14:04:07 +0530

    mninor changes

--- Transaction-Service/src/main/java/com/stackroute/com/TransactionService/TransactionServiceApplication.java
+++ Transaction-Service/src/main/java/com/stackroute/com/TransactionService/TransactionServiceApplication.java
@@ -2,6 +2,7 @@ package com.stackroute.com.TransactionService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
