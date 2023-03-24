08b14c6f5a2ac6401be1db34d0531b29a68d058f
Durga <durga.sathyanps@batonsystems.com>
2023-03-24 14:48:10 +0530

    icons added in login and signup

--- ProductWebapp/webapp/src/pages/SignUp.jsx
+++ ProductWebapp/webapp/src/pages/SignUp.jsx
@@ -8,12 +8,15 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import KeyIcon from '@mui/icons-material/Key';
 import img from "../images/swift_image.jpg";
 import { MuiTelInput } from "mui-tel-input";
 import axios from "axios";
-import { Card } from '@mui/material';
+import { Card, InputAdornment } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { style } from "@mui/system";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 const theme = createTheme();
 export default function SignUp() {
   const navigate = useNavigate();
@@ -112,6 +115,13 @@ export default function SignUp() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
               />
               <TextField
                 margin="normal"
@@ -122,6 +132,13 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                }}
               />
               <TextField
                 margin="normal"
@@ -131,6 +148,13 @@ export default function SignUp() {
                 label="Name"
                 name="name"
                 autoComplete="name"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <DriveFileRenameOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
               />
               <MuiTelInput
                 margin="normal"
