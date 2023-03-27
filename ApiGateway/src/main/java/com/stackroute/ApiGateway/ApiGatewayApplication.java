598a6a4e22cc9469008179de06abca6ca21ddd61
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-27 13:48:25 +0530

    Working on CORS integration

--- ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -35,5 +37,14 @@ public class ApiGatewayApplication {
 
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 }
