8604538db0d44f82e545bb61ac9f3b6503fbde09
shonestalin <shone.stalin@batonsystems.com>
2023-03-27 13:10:59 +0530

    minor changes

--- ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
--- ProductWebapp/webapp/src/pages/SignUp.jsx
+++ ProductWebapp/webapp/src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignUp() {
     };
     console.log(mapData);
     axios
-      .post("http://localhost:8090/user-service/register", mapData)
+      .post("http://localhost:8080/user-service/register", mapData)
       .then((res) => {
         if (res.status == 201) {
           navigate("/");
@@ -54,13 +54,8 @@ export default function SignUp() {
       <Card
         style={{
           padding: "100px",
-<<<<<<< HEAD
-          height: "80vh",
-          
-=======
           height: "85vh",
           overflow: "auto",
->>>>>>> 1589eaba384d98aaa68700cbd41b966e86777511
           borderColor: "black",
           border: "2px solid grey",
         }}
@@ -98,7 +93,7 @@ export default function SignUp() {
                 width: "100%",
                 height: "100%",
                 objectFit: "cover",
-                padding: "5px"
+                padding: "5px",
               }}
             />
           </Grid>
