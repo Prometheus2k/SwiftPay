f049d76ebeb88e971cf01a9717c938a0d1f17505
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-27 13:09:33 +0530

    Added cross origin for api gateway

--- ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/stackroute/ApiGateway/ApiGatewayApplication.java
@@ -28,6 +28,9 @@ public class ApiGatewayApplication {
 				.route(r -> r.path("/transaction-service/**")
 						.uri("lb://TRANSACTION-SERVICE")
 				)
+				.route(r -> r.path("/product-webapp-service/**")
+						.uri("lb://PRODUCT-WEBAPP-SERVICE")
+				)
 				.build();
 
 	}
