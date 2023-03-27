688e49c642a4ba8fa10dad2aefd8fd78a4b6d68b
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-27 10:20:26 +0530

    Added files to bank service

--- Bank-Service/src/main/java/com/stackroute/com/BankService/interservice/InterService.java
+++ Bank-Service/src/main/java/com/stackroute/com/BankService/interservice/InterService.java
@@ -1,17 +1,52 @@
 package com.stackroute.com.BankService.interservice;
 
-import com.stackroute.com.BankService.models.User;
+import com.stackroute.com.BankService.model.User;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+
+import java.net.http.HttpHeaders;
 
 @Service
 public class InterService {
 
     public User getUserDetails(String token) {
         RestTemplate restTemplate = new RestTemplate();
-        String uri = "http://localhost:8090/user-service/test/";
-        User user = restTemplate.getForObject(uri + token, User.class);
-        return user;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("token", token);
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+        String uri = "http://localhost:8090/user-service/interservice/get/user";
+        ResponseEntity<User> entity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, User.class);
+        return entity.getBody();
     }
+
+    public boolean verifyUser(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("token", token);
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+        String uri = "http://localhost:8090/user-service/interservice/verify/";
+        ResponseEntity<Boolean> entity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Boolean.class);
+        boolean check = entity.getBody();
+        return check;
+    }
+
+    public boolean initiateTransaction(String MT101) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("MT101", MT101);
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        String uri = "http://localhost:8060/transaction-service/transfer";
+        ResponseEntity<Boolean> entity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Boolean.class);
+        return entity.getBody();
+    }
+
 
 }
