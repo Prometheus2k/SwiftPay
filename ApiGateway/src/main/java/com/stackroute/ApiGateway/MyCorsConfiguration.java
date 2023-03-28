06ec3fd7c9d7372aaaf7f4493c72e63e91e994cd
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-28 14:06:46 +0530

    Merging

--- ApiGateway/src/main/java/com/stackroute/ApiGateway/MyCorsConfiguration.java
+++ ApiGateway/src/main/java/com/stackroute/ApiGateway/MyCorsConfiguration.java
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-//package com.stackroute.ApiGateway;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.server.reactive.ServerHttpRequest;
-//import org.springframework.http.server.reactive.ServerHttpResponse;
-//import org.springframework.web.cors.reactive.CorsUtils;
-//import org.springframework.web.server.ServerWebExchange;
-//import org.springframework.web.server.WebFilter;
-//import org.springframework.web.server.WebFilterChain;
-//import reactor.core.publisher.Mono;
-//@Configuration
-//public class MyCorsConfiguration {
-//    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
-//    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
-//    private static final String ALLOWED_ORIGIN = "http://localhost:3000/";
-//    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
-//    @Bean
-//    public WebFilter corsFilter() {
-//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-//            ServerHttpRequest request = ctx.getRequest();
-//            if (CorsUtils.isCorsRequest(request)) {
-//                ServerHttpResponse response = ctx.getResponse();
-//                HttpHeaders headers = response.getHeaders();
-//                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-//                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-//                headers.add("Access-Control-Max-Age", MAX_AGE);
-//                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
-//                if (request.getMethod() == HttpMethod.OPTIONS) {
-//                    response.setStatusCode(HttpStatus.OK);
-//                    return Mono.empty();
-//                }
-//            }
-//            return chain.filter(ctx);
-//        };
-//    }
-//}
-=======
 package com.stackroute.ApiGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -56,7 +15,7 @@ import reactor.core.publisher.Mono;
 public class MyCorsConfiguration {
     private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
-    private static final String ALLOWED_ORIGIN = "http://localhost:3000";
+    private static final String ALLOWED_ORIGIN = "http://localhost:3000/";
     private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
     @Bean
     public WebFilter corsFilter() {
@@ -78,4 +37,3 @@ public class MyCorsConfiguration {
         };
     }
 }
->>>>>>> e4b1a5d1086c019b4cb3d3bd5e947ba4dfe05b94
