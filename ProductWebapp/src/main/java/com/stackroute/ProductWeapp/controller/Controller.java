53f85382d7ea08800e28d68c33d2cf5f17fac38f
Adityakrishnan <adityakrishnan@batonsystems.com>
2023-03-28 09:48:09 +0530

    Added CORS Configuration

--- ProductWebapp/src/main/java/com/stackroute/ProductWeapp/controller/Controller.java
+++ ProductWebapp/src/main/java/com/stackroute/ProductWeapp/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.stackroute.ProductWeapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("product-webapp")
 public class Controller {
     public String webapp() {
         return "index.html";
